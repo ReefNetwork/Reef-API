6e1663f4e66d1e55517c12c79b9fd5663650e33b
ree-jp <mail@ree-jp.net>
2022-02-23 01:45:00 +0900

    rate limitを設定

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,30 @@
 import * as admin from "firebase-admin";
 import * as express from "express";
 import * as functions from "firebase-functions";
+import {FirebaseFunctionsRateLimiter} from "firebase-functions-rate-limiter";
 
 import * as v1 from "./v1/";
 
 admin.initializeApp();
 
 const app = express();
+const database = admin.firestore();
+
+const limiter = FirebaseFunctionsRateLimiter.withFirestoreBackend({
+  name: "ip-rate-limit",
+  maxCalls: 3,
+  periodSeconds: 3,
+}, database);
 
 app.use("/v1/", v1.router);
 
-// noinspection JSUnusedGlobalSymbols
-export const http = functions.region("asia-northeast1").https.onRequest(app);
+const http = functions.region("asia-northeast1").https;
+
+export const api = http.onRequest(async (req, res) => {
+  if (await limiter.isQuotaExceededOrRecordUsage("host-" + req.hostname)) {
+    res.status(429).json({error: "Too many requests"});
+    return;
+  }
+
+  app(req, res);
+});
