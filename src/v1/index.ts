ab1096916e2f940737e50d0d6a8fe28e2a569850
ree-jp <mail@ree-jp.net>
2022-03-05 02:11:25 +0900

    tokenの発行を実装

--- src/v1/index.ts
+++ src/v1/index.ts
@@ -1,10 +1,12 @@
 import * as express from "express";
 
+import * as login from "./login";
 import * as xuid from "./xuid";
 import * as ranking from "./ranking";
 
 // eslint-disable-next-line new-cap
 export const router = express.Router();
 
+router.use("/login", login.router);
 router.use("/xuid", xuid.router);
 router.use("/ranking", ranking.router);
